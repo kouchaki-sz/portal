6839adfce3726e51530ee09e6712a5159c69b297
kouchaki-sz <kou.chaki.539@gmail.com>
2026-06-03 14:42:32 +0900

    GASデプロイURL更新（時刻対応版）

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // GASのウェブアプリURLをデプロイ後にここに設定してください
 // ============================================================
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxS-tM981mgmcrDarQ3cpipyhrdr4Z7ha8OHWVYmswIRaZcBbNEfzteXvEMOWEICfwP/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzGnkH9Qz014Q3IVVTPuRgVaBa7JnVDKpla_4pFcrGsYv402T-bOuQ2k3yXx8x7FAtk/exec',
   APP_NAME: '情報共有ポータル',
   ORG_NAME: 'チームポータル'
 };
